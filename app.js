621d1eb1e29c4a6501e8229bf5efefbbf21b0c48
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2023-11-04 07:13:33 +0530

    implemented detection of invalid news

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 //Libraries
 const express = require("express")
 const app = express()
+const cors=require("cors")
+app.use(cors())
 app.use(express.json({ limit: "50mb" }))
 
 //Controllers
 const newsController = require("./controllers/newsAPI");
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/google/flan-t5-base",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
+}
 
-
+query({"inputs": "respond yes if the text contains information which seems to talk about israel palestine war then respond yes.\nText: Israel is bombing palestine"}).then((response) => {
+	console.log(JSON.stringify(response));
+});
 //Constants
 let source = []
 newsController.fetchSources().then((response) => {
@@ -14,9 +30,9 @@ newsController.fetchSources().then((response) => {
 })
 const category = newsController.fetchCategories();
 
+
 app.post('/getNews', (req, res, next) => {
     if (req.body.sources.length === 0) {
-        console.log(source);
         for (var key in source) {
             req.body.sources.push(source[key]);
         }
--- controllers/newsAPI.js
+++ controllers/newsAPI.js
@@ -3,15 +3,51 @@ const NewsAPI = require('newsapi');
 require('dotenv').config({ path: path.resolve(__dirname, "../.env") });
 
 const newsapi = new NewsAPI(process.env.API_KEY);
-
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/google/flan-t5-base",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
+}
 const fetchNews = (req, res) => {
-    //Fetch top headlines from the newsapi
+    let restrictive_prompt="respond yes else no If the text contains information which seems to talk about ";
+    for(var val in req.body.restrictions)
+    {
+        restrictive_prompt=restrictive_prompt+req.body.restrictions[val]+" ";
+    }
+    restrictive_prompt=restrictive_prompt+".";
+    // Fetch top headlines from the newsapi
     newsapi.v2.topHeadlines({
         sources: req.body.sources.join(","),
         language: 'en',
         categories: req.body.sources.join(",")
-    }).then(response => {
-        res.status(200).json(response);
+    }).then(async response => {
+       let articles=response.articles;
+       let new_response=[];
+       for(var val in articles)
+       {
+        let prompt=articles[val].description;
+        final_prompt=restrictive_prompt+prompt;
+        console.log(final_prompt)
+        ans=await query({"inputs":final_prompt});
+        console.log(ans);
+        if(ans[0].generated_text=="no")
+            {
+                new_response.push(articles[val]);
+            }
+       }
+       if(req.body.restrictions.length)
+       {
+      res.status(200).json(new_response);
+       }
+            else
+      res.status(200).json(response);
     });
 
 }
@@ -25,7 +61,7 @@ const fetchCategories = () => {
     }).then(response => {
         //Create and return a set containing all categories available in the API
         categories = new Set();
-        for (var k of response.sources) {
+        for (var k in response.sources) {
             categories.add(k.category);
         }
         return Array.from(categories);
